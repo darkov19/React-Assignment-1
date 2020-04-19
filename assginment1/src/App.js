75de90270c29d6e2a30beb2f0dd26fb197750cd7
Viral Shsatri <shastriv19@gmail.com>
2020-04-19 14:39:31 +0530

    Initial Setup

---
+++ assginment1/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./App.css";
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <UserInput />
+                <UserOutput />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ assginment1/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutput = () => {
+    return (
+        <div>
+            <h1>Name:</h1>
+            <p> Viral</p>
+        </div>
+    );
+};
+
+export default userOutput;
